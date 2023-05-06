c50f58c52662046325722ffea26547c7c2e03cc1
P0sterNutbag <tkeys2112@gmail.com>
2023-05-05 20:58:19 -0600

    Added boost/flight and fixed gravity

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,15 +14,20 @@ public class PlayerMovement : MonoBehaviour
     public float maxSlideAngle = 90f;
     public float turnSpeed = 180f; // for tank controls
     public float groundDistance = 0.4f;
+    public float boostSpeed = 0f;
+    public float boostAcceleration = 0.02f;
+    public float boostMax = 12f;
 
     public Transform groundCheck;
     public LayerMask groundMask;
     public bool isHolding = false;
-    public bool isTankControls = true;
+    public bool isTankControls = false;
     private float moveDirection = 0;
     private Quaternion moveRotation;
 
     private bool isSliding = false;
+    private bool isBoosting = false;
+    private bool isBoostVertical = false;
 
     public enum State
     { 
@@ -49,21 +54,52 @@ public class PlayerMovement : MonoBehaviour
                     velocity.y = -2f;
                 }
 
+                // get move inputs
+                float horizontal = Input.GetAxis("Horizontal");
+                float vertical = Input.GetAxis("Vertical");
+
+                // determine boost direction
+                if (Input.GetKeyDown("left shift"))
+                {
+                    if ((horizontal == 0 && vertical == 0)) //|| (!isGrounded))
+                    {
+                        isBoostVertical = true;
+                    }
+                    else
+                    {
+                        isBoostVertical = false;
+                    }
+                }
+
+                // boost
+                if (Input.GetKey("left shift"))
+                {
+                    boostSpeed += boostAcceleration;
+                    isBoosting = true;
+                }
+                else if (boostSpeed > 0)
+                { 
+                    boostSpeed -= boostAcceleration;
+                    isBoosting = false;
+                }
+                boostSpeed = Mathf.Clamp(boostSpeed, 0, boostMax);
+
                 // move
                 if (!isTankControls)
                 {
-                    float horizontal = Input.GetAxis("Horizontal");
-                    float vertical = Input.GetAxis("Vertical");
-
                     Vector3 move = transform.right * horizontal + transform.forward * vertical;
 
-                    controller.Move(move * speed * Time.deltaTime);
+                    controller.Move(move * (speed+boostSpeed) * Time.deltaTime);
+
+                    // boost up if not moving
+                    if (isBoostVertical)
+                    {
+                        move = Vector3.up * boostSpeed * Time.deltaTime;
+                        controller.Move(move);
+                    }
                 }
                 else 
                 {
-                    float horizontal = Input.GetAxis("Horizontal");
-                    float vertical = Input.GetAxis("Vertical");
-
                     moveDirection += horizontal * turnSpeed * Time.deltaTime;
                     moveRotation = Quaternion.AngleAxis(moveDirection, Vector3.up);
                     Vector3 movementVector = moveRotation * Vector3.forward * vertical * speed * Time.deltaTime;
@@ -71,6 +107,7 @@ public class PlayerMovement : MonoBehaviour
 
                     Debug.Log(moveDirection * vertical);
                 }
+
                 // jump
                 if (Input.GetButtonDown("Jump") && isGrounded)
                 {
@@ -78,7 +115,14 @@ public class PlayerMovement : MonoBehaviour
                 }
 
                 // change and apply gravity
-                velocity.y += gravity * Time.deltaTime;
+                if (!isGrounded && !isBoosting)
+                {
+                    velocity.y += gravity * Time.deltaTime;
+                }
+                else
+                {
+                    velocity.y = 0;
+                }
 
                 controller.Move(velocity * Time.deltaTime);
 
