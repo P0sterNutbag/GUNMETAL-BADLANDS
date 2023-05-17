d32be2fb92d1dd242769af67712cbc4708ebe0f9
P0sterNutbag <tkeys2112@gmail.com>
2023-05-16 23:24:47 -0600

    Tried fixing some bullet collision but it didnt really work lol

--- Assets/Scripts/Player/Bullet.cs
+++ Assets/Scripts/Player/Bullet.cs
@@ -5,12 +5,15 @@ public class Bullet : MonoBehaviour
     public float lifetime = 2f;
     public int damage = 10;
     public GameObject owner;
+
+    private Vector3 positionLast;
     //public float speed;
 
     private Rigidbody rb;
 
     void Start()
     {
+        positionLast = transform.position;
         // Get the rigidbody component of the bullet object
         rb = GetComponent<Rigidbody>();
 
@@ -20,7 +23,37 @@ public class Bullet : MonoBehaviour
         // Destroy the bullet after its lifetime has expired
         Destroy(gameObject, lifetime);
     }
+    /*
+    private void FixedUpdate()
+    {
+        RaycastHit other;
+        if (Physics.Raycast(transform.position, positionLast - transform.position, out other))
+        {
+            if (other.collider.isTrigger == false)
+            {
+                // If the bullet collides with an object that has a Health component, damage it
+                GameObject enemy = other.collider.gameObject;
+                if (enemy.CompareTag("Enemy"))
+                {
+                    EnemyHealth health = enemy.GetComponent<EnemyHealth>();
+                    if (health != null)
+                    {
+                        health.TakeDamage(damage);
 
+                        Debug.Log("now thats what i call a hit");
+                    }
+                }
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        positionLast = transform.position;
+    }
+    */
+    
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject != owner)
@@ -36,4 +69,5 @@ public class Bullet : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    
 }
