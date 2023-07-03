007c7764a77c141155c39c8eb7c9ee72b9dc8f55
P0sterNutbag <91211605+P0sterNutbag@users.noreply.github.com>
2023-07-03 14:57:46 -0600

    Created topdown movement script

---
+++ Assets/Scripts/Player/OverheadMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Legs stats;
+    
+    [HideInInspector]
+    public float moveDirection = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the mouse position
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = transform.position.z; // Match the player's z-position
+
+        // Rotate the player towards the mouse using LookAt
+        transform.LookAt(mousePosition);
+
+        // Get movement variables
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // determine speed
+        if (vertical > 0)
+        {
+            currentSpeed += stats.accelerationSpd * Time.deltaTime;
+        }
+        else if (vertical < 0)
+        {
+            currentSpeed -= stats.decelerationSpd * Time.deltaTime;
+        }
+        currentSpeed = Mathf.Clamp(currentSpeed, -stats.speed, stats.speed);
+        
+        // determine direction
+        moveDirection += horizontal * stats.turnSpeed * Time.deltaTime;
+        moveRotation = Quaternion.AngleAxis(moveDirection, Vector3.up);
+        Vector3 movementVector = moveRotation * Vector3.forward * currentSpeed * Time.deltaTime;
+        if (isGrounded && !isBoosting) { movementVector.y = -4.5f; } // get rid of bumps down slopes
+        
+        // actually move
+        controller.Move(movementVector);
+    }
+}
