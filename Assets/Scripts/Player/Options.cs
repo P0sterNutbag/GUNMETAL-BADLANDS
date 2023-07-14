063ebfd0e36bc77855108fd980e4c23719e81b58
P0sterNutbag <91211605+P0sterNutbag@users.noreply.github.com>
2023-07-14 12:55:34 -0600

    Created Options class

---
+++ Assets/Scripts/Player/Options.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerOptions : MonoBehaviour
+{
+    public lineSpacing = 1;
+
+    string[] = {"Inventory", "Exit to Open World", "Options"};
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    
+}
